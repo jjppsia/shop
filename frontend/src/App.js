557d65e61654ed48da4bcd99aa4ceb09f5f82063
John Paul Sia <johnpaulsia4@gmail.com>
2022-11-14 03:37:14 +0800

    Add react-router-bootrap and react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import { Header, Footer } from './components'
-import { HomePage } from './pages'
+import { HomePage, ProductPage } from './pages'
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomePage />
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/product/:id' element={<ProductPage />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
