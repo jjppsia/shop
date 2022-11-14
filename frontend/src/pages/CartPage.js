f2eb299f75152dfa13bb593c0721b3ce28173c31
John Paul Sia <johnpaulsia4@gmail.com>
2022-11-14 19:04:45 +0800

    Add cart functionality

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+// import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+/* import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { Message } from '../components' */
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
+import { addToCart } from '../actions/cartActions'
 
 function CartPage() {
+  const { id } = useParams()
+  const [searchParams] = useSearchParams()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { cartItems } = useSelector((state) => state.cart)
+
+  const qty = searchParams.get('qty') ? Number(searchParams.get('qty')) : 1
+
+  useEffect(() => {
+    if (!id) {
+      return
+    }
+
+    dispatch(addToCart(id, qty))
+  }, [dispatch, id, qty])
+
   return <div>CartPage</div>
 }
 
