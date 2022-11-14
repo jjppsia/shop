19a18d965dea4df389a3afaff6911ead30d625af
John Paul Sia <johnpaulsia4@gmail.com>
2022-11-14 14:29:27 +0800

    Add seeder file to populate db with mock data

---
+++ backend/src/seeder.js
@@ -0,0 +1,52 @@
+import 'dotenv/config'
+import chalk from 'chalk'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+import users from './data/users.js'
+import Order from './models/orderModel.js'
+import Product from './models/productModel.js'
+import User from './models/userModel.js'
+
+await connectDB()
+
+const importData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    const createdUsers = await User.insertMany(users)
+    const adminUser = createdUsers[0]._id
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser }
+    })
+
+    await Product.insertMany(sampleProducts)
+
+    console.log(`✔️  ${chalk.greenBright('Data Imported!')}`)
+    process.exit()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    console.log(`✔️  ${chalk.redBright('Data Destroyed!')}`)
+    process.exit()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+if (process.argv[2] === '-d') {
+  destroyData()
+}
+
+importData()
