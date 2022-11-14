53e6c035ead51a47c414a03cca522c711991f2b9
John Paul Sia <johnpaulsia4@gmail.com>
2022-11-14 15:57:02 +0800

    Add the productListReducer, actions, and constants

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
 import { composeWithDevTools } from '@redux-devtools/extension'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import { productListReducer } from './reducers/productReducers'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  productList: productListReducer,
+})
 
 const initialState = {}
 
