10a8177beb111675e55022d610a74b3ed802053c
John Paul Sia <johnpaulsia4@gmail.com>
2022-11-14 20:25:25 +0800

    Update cart page

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-/* import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
-import { Message } from '../components' */
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import { Message } from '../components'
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
 import { addToCart } from '../actions/cartActions'
 
@@ -23,7 +23,93 @@ function CartPage() {
     dispatch(addToCart(id, qty))
   }, [dispatch, id, qty])
 
-  return <div>CartPage</div>
+  const handleRemoveFromCart = (id) => {
+    console.log('Removed')
+  }
+
+  const handleCheckout = () => {
+    navigate('/login?redirect=shipping')
+  }
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            {' '}
+            Your cart is empty. <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.id}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>
+                    <p>${item.price}</p>
+                  </Col>
+                  <Col md={2}>
+                    <Form.Select
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(addToCart(item.id, Number(e.target.value)))
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => handleRemoveFromCart(item.id)}
+                    >
+                      <i className='fas fa-trash' />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              $
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item className='d-grid gap-2'>
+              <Button
+                type='button'
+                disabled={cartItems.length === 0}
+                onClick={handleCheckout}
+              >
+                Proceed To Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  )
 }
 
 export default CartPage
