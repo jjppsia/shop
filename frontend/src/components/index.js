0aacdab148454918a29f71ea8c8cbebd3e2175b8
John Paul Sia <johnpaulsia4@gmail.com>
2022-11-14 16:27:58 +0800

    Add Loader and Message component

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,6 @@
-export { default as Header } from './Header'
 export { default as Footer } from './Footer'
+export { default as Header } from './Header'
+export { default as Loader } from './Loader'
+export { default as Message } from './Message'
 export { default as Product } from './Product'
 export { default as Rating } from './Rating'
