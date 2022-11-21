1333b611963148c71891baa49b23102c45028388
John Paul Sia <johnpaulsia4@gmail.com>
2022-11-21 19:27:44 +0800

    Update backend's index file

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,13 +11,17 @@ import productRoutes from './routes/productRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 
-const __dirname = path.resolve()
-
 const NODE_ENV = process.env.NODE_ENV
 const PORT = process.env.PORT || 5000
 const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID
 
+const __dirname = path.resolve()
+
 const app = express()
+
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
@@ -28,13 +32,9 @@ app.use('/api/v1/orders', verifyToken, orderRoutes)
 app.use('/api/v1/upload', verifyToken, adminAuth, uploadRoutes)
 app.get('/api/v1/config/paypal', (req, res) => res.send(PAYPAL_CLIENT_ID))
 
-if (NODE_ENV === 'development') {
-  app.use(morgan('dev'))
-}
-
 if (NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html')))
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 }
 
 const start = async () => {
