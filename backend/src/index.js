bf9b9f17c64bc5e3f8dfab672d6bd09b8fd7a72a
John Paul Sia <johnpaulsia4@gmail.com>
2022-11-21 17:23:00 +0800

    Add conditions if node_env is in development or production

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk'
 import 'dotenv/config'
 import express from 'express'
-import logger from 'morgan'
+import morgan from 'morgan'
 import path from 'path'
 
 import connectDB from './config/db.js'
@@ -11,22 +11,31 @@ import productRoutes from './routes/productRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 
-const NODE_ENV = process.env.NODE_ENV || 'development'
+const __dirname = path.resolve()
+
+const NODE_ENV = process.env.NODE_ENV
 const PORT = process.env.PORT || 5000
 const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID
 
 const app = express()
 
-app.use(logger('dev'))
 app.use(express.json())
 
-app.use('/uploads', express.static(path.join(path.resolve(), '/uploads')))
-
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 app.use('/api/v1/products', productRoutes)
 app.use('/api/v1/users', userRoutes)
 app.use('/api/v1/orders', verifyToken, orderRoutes)
 app.use('/api/v1/upload', verifyToken, adminAuth, uploadRoutes)
 app.get('/api/v1/config/paypal', (req, res) => res.send(PAYPAL_CLIENT_ID))
+
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+if (NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html')))
+}
 
 const start = async () => {
   try {
